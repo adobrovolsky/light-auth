162b681ba20e75a6209bd4f1a6986db335dba93e
Andrey Dobrovolskiy <andrey.g.dobrovolskiy@gmail.com>
2017-02-13 00:44:09 +0200

    Configure project for a Spring MVC support

---
+++ src/main/java/dobrovolsky/HomeController.java
@@ -0,0 +1,16 @@
+package dobrovolsky;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getHello(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        return "index";
+    }
+}
---
+++ web/WEB-INF/pages/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8 />
+    <title>Light Auth</title>
+  </head>
+  <body>
+    <h2>${message}</h2>
+  </body>
+</html>
